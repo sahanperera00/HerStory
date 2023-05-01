567ff53a1189455df4409f9154b66aa976a87f50
NashaliP <nashaliperera0@gmail.com>
2023-05-01 22:41:54 +0530

    added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Forum from "./pages/Forum/Forum";
+import Community from "./pages/Forum/Community";
 import AdminDashboard from "./pages/AdminDashboard/AdminDashboard";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import ManageConsultants from "./pages/AdminDashboard/ManageConsultants";
@@ -12,10 +13,17 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/forum" element={<Forum />} />
+        <Route path="/community" element={<Community />} />
 
         <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/admin/manage-consultants" element={<ManageConsultants />} />
-        <Route path="/admin/category-management" element={<CategoryManagement />} />
+        <Route
+          path="/admin/manage-consultants"
+          element={<ManageConsultants />}
+        />
+        <Route
+          path="/admin/category-management"
+          element={<CategoryManagement />}
+        />
         <Route path="/admin/manage-forum" element={<Forum />} />
       </Routes>
     </div>
