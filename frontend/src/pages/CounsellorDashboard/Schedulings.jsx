0fe11e44f17c7cf4ff24d7b6d5944106be18f62b
Sahan Perera <85289620+sahanperera00@users.noreply.github.com>
2023-05-04 17:44:11 +0530

    schedulings page for counsellor dashboard created

---
+++ frontend/src/pages/CounsellorDashboard/Schedulings.jsx
@@ -0,0 +1,110 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useStateContext } from "../../contexts/ContextProvider";
+import { FiSettings } from "react-icons/fi";
+import { Navbar, Footer, ThemeSettings } from "../../components";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import CounsellorSidebar from "./CounsellorSidebar";
+import { Header, TableHeader, TableData } from "../../components";
+
+/* IMPORT ALL YOUR IMPORTS AS USUAL ABOVE HERE, REMOVE UNNECESSARY ONES*/
+
+export default function Schedulings() {
+  // <== THIS IS THE COMPONENT NAME, CHANGE IT TO YOUR COMPONENT NAME
+
+  const {
+    setCurrentColor,
+    setCurrentMode,
+    currentMode,
+    activeMenu,
+    currentColor,
+    themeSettings,
+    setThemeSettings,
+  } = useStateContext();
+
+  /* 
+  ------------------------------------------------
+  YOUR AXIOS CALLS AND USE STATES GOES  ABOVE HERE 
+  ------------------------------------------------
+  */
+
+  const orders = [
+    {
+      id: 1,
+      client: "John Doe",
+      grossPrice: "1000",
+      commission: "100",
+      status: "Confirmed",
+    },
+    {
+      id: 2,
+      client: "John Doe",
+      grossPrice: "1000",
+      commission: "100",
+      status: "Confirmed",
+    },
+    {
+      id: 3,
+      client: "John Doe",
+      grossPrice: "1000",
+      commission: "100",
+      status: "Confirmed",
+    },
+  ];
+
+  return (
+    <div>
+      <div className={currentMode === "Dark" ? "dark" : ""}>
+        <div className="flex relative dark:bg-main-dark-bg bg-gradient-to-t from-[#ccb1b1] to-[#ffdede]">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            {" "}
+            {/* THEME SETTINGS BUTTON */}
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                type="button"
+                onClick={() => setThemeSettings(true)}
+                style={{ background: currentColor, borderRadius: "50%" }}
+                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+
+          {activeMenu ? ( // SIDEBAR IMPLEMENTATION
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-[#f9e9e9]">
+              <CounsellorSidebar />
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              <CounsellorSidebar />
+            </div>
+          )}
+
+          <div
+            className={
+              activeMenu
+                ? "dark:bg-main-dark-bg min-h-screen md:ml-72 w-full  "
+                : "dark:bg-main-dark-bg w-full min-h-screen flex-2 "
+            }
+          >
+            {/* NAVBAR IMPLEMENTATION */}
+            <div className="fixed md:static dark:bg-main-dark-bg w-full ">
+              <Navbar />
+            </div>
+
+            <div>
+              {themeSettings && <ThemeSettings />}
+              <div className="md:m-6 p-5">
+                <Header title="Schedulings" />
+
+                
+              </div>
+            </div>
+            <Footer />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
