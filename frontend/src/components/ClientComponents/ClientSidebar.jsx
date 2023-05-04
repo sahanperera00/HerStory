600be4e6dcc7195607d79cabe436f4d7c436bea1
DevinduSamarasinghe <devindu990@gmail.com>
2023-05-04 17:06:41 +0530

    +ClientSidebar Update

--- frontend/src/components/ClientComponents/ClientSidebar.jsx
+++ frontend/src/components/ClientComponents/ClientSidebar.jsx
@@ -1,8 +1,13 @@
-import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import { MdOutlineCancel } from "react-icons/md";
-import { FiUser } from "react-icons/fi";
-
+import { FiUser,FiSettings } from "react-icons/fi";
+import { BsPeople } from "react-icons/bs";
+import {CgCommunity} from "react-icons/cg"
+import {SlDiamond} from "react-icons/sl";
+ 
+import {AiOutlineForm} from "react-icons/ai"
+import {CiSettings} from "react-icons/ci"
+import { MdOutlineForum } from "react-icons/md";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import { useStateContext } from "../../contexts/ContextProvider";
 
@@ -17,21 +22,21 @@ export default function ClientSidebar() {
   };
 
   const activeLink =
-    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2";
+    "flex items-center gap-5 px-4 pt-3 pb-2.5 rounded-lg  text-white text-md my-3";
   const normalLink =
-    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2 transition duration-300 ease-in-out";
+    "flex items-center gap-5 px-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700  my-3 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray transition duration-300 ease-in-out";
 
   return (
-    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
+    <div className=" h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
       {activeMenu && (
         <>
-          <div className="flex justify-between items-center">
-            <Link
-              to="/"
-              onClick={handleCloseSideBar}
-              className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
-            >
-              Vedra
+          <div className="flex justify-center items-center">
+            <Link to="/" onClick={handleCloseSideBar}>
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/herstory-6a3c0.appspot.com/o/logo-no-background.png?alt=media&token=08cba1bc-5127-4a4a-8ea6-75cf010b01b1"
+                alt="herstory logo"
+                className="w-[200px] my-10"
+              />
             </Link>
 
             <TooltipComponent content="Menu" position="BottomCenter">
@@ -46,45 +51,107 @@ export default function ClientSidebar() {
             </TooltipComponent>
           </div>
 
-          <div className="mt-10 ">
+          <div className="mt-5 mx-5">
+            <p className="text-[12px] text-gray-400 dark:text-gray-400 mt-4 uppercase">
+              Services
+            </p>
+
+
             <NavLink
-              to="/"
+              to="/coming-soon"
               onClick={handleCloseSideBar}
               style={({ isActive }) => ({
                 backgroundColor: isActive ? currentColor : "",
               })}
               className={({ isActive }) => (isActive ? activeLink : normalLink)}
             >
-              <FiUser /> {/*  icon  */}
-              <span className="capitalize ">Manage Consultants</span>{" "}
+              <AiOutlineForm /> {/*  icon  */}
+              <span className="capitalize ">Official Complaints</span>
+              {/*  link name  */}
+            </NavLink>
+              
+            <NavLink
+              to="/community"
+              onClick={handleCloseSideBar}
+              style={({ isActive }) => ({
+                backgroundColor: isActive ? currentColor : "",
+              })}
+              className={({ isActive }) => (isActive ? activeLink : normalLink)}
+            >
+              <CgCommunity /> {/*  icon  */}
+              <span className="capitalize ">Her-Story Community</span>{" "}
               {/*  link name  */}
             </NavLink>
 
             <NavLink
-              to="/"
+              to="/counsel"
               onClick={handleCloseSideBar}
               style={({ isActive }) => ({
                 backgroundColor: isActive ? currentColor : "",
               })}
               className={({ isActive }) => (isActive ? activeLink : normalLink)}
             >
-              <FiUser /> {/*  icon  */}
-              <span className="capitalize ">Category Management</span>{" "}
+              <BsPeople /> {/*  icon  */}
+              <span className="capitalize ">Her-Story Counsellor</span>{" "}
+              {/*  link name  */}
+            </NavLink>
+
+            <p className="text-[12px] text-gray-400 dark:text-gray-400 mt-4 uppercase">
+              HerStory+
+            </p>
+
+            <NavLink
+              to="/admin/updateProfile"
+              onClick={handleCloseSideBar}
+              style={({ isActive }) => ({
+                backgroundColor: isActive ? currentColor : "",
+              })}
+              className={({ isActive }) => (isActive ? activeLink : normalLink)}
+            >
+              <SlDiamond /> {/*  icon  */}
+              <span className="capitalize ">Become a Member</span>{" "}
               {/*  link name  */}
             </NavLink>
 
             <NavLink
-              to="/"
+              to="/admin/manage-post"
               onClick={handleCloseSideBar}
               style={({ isActive }) => ({
                 backgroundColor: isActive ? currentColor : "",
               })}
               className={({ isActive }) => (isActive ? activeLink : normalLink)}
             >
-              <FiUser /> {/*  icon  */}
-              <span className="capitalize ">Forum Management</span>{" "}
+              <MdOutlineForum /> {/*  icon  */}
+              <span className="capitalize ">Talk with friendsd</span>{" "}
               {/*  link name  */}
             </NavLink>
+
+            <p className="text-[12px] text-gray-400 dark:text-gray-400 mt-4 uppercase">
+              Settings
+            </p>
+            <NavLink
+              to="/updateProfile"
+              onClick={handleCloseSideBar}
+              style={({ isActive }) => ({
+                backgroundColor: isActive ? currentColor : "",
+              })}
+              className={({ isActive }) => (isActive ? activeLink : normalLink)}
+            >
+              <FiSettings /> {/*  icon  */}
+              <span className="capitalize ">Settings</span>{" "}
+              {/*  link name  */}
+            </NavLink>
+
+            <div id="dropdown-cta" class="p-4 mt-6 rounded-lg bg-blue-50 dark:bg-blue-900 mr-4" role="alert">
+              <div class="flex items-center mb-3">
+                <span class="bg-orange-100 text-orange-800 text-sm font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-orange-200 dark:text-orange-900">Beta</span>
+
+              </div>
+              <p class="mb-3 text-sm text-blue-800 dark:text-blue-400">
+                The current version of HerStory is a prototype and we will be back with a bang! 
+              </p>
+              <a class="text-sm text-blue-800 underline hover:text-blue-900 dark:text-blue-400 dark:hover:text-blue-300" href="https://www.linkedin.com/in/devindusamarasinghe/">Project by Code Crusaders</a>
+            </div>
           </div>
         </>
       )}
