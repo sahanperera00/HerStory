46a3ddf95eeca2bfbfe3f78db494876ff1d3efdd
Sahan Perera <85289620+sahanperera00@users.noreply.github.com>
2023-05-04 13:42:04 +0530

    changed text alignment

--- frontend/src/components/Table/TableData.jsx
+++ frontend/src/components/Table/TableData.jsx
@@ -1,6 +1,6 @@
 export default function TableData({ value }) {
   return (
-    <td className="text-center px-3 align-middle border-l-0 border-r-0 text-m whitespace-nowrap p-3">
+    <td className="text-left px-3 align-middle border-l-0 border-r-0 text-m whitespace-nowrap p-3">
       {value}
     </td>
   );
