bd76063ed58a90a24456a01fb36712704bec2919
Sahan Perera <85289620+sahanperera00@users.noreply.github.com>
2023-05-07 02:34:54 +0530

    Feedback Model created

---
+++ backend/models/counselling/feedback/feedback.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const feedbackSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    review: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Feedback = mongoose.model("Feedback", feedbackSchema);
+export default Feedback;
