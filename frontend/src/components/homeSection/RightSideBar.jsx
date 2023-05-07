63fcf81e176ac1858ade8598edd28e192260ec3c
Sahan Perera <85289620+sahanperera00@users.noreply.github.com>
2023-05-07 18:14:02 +0530

    styles added to the forum

--- frontend/src/components/homeSection/RightSideBar.jsx
+++ frontend/src/components/homeSection/RightSideBar.jsx
@@ -3,7 +3,7 @@ import RightSideBarContainer from "./rightSideBarComponents/RightSideBarContaine
 import RightSideBarContainer2 from "./rightSideBarComponents/RightSideBarContainer2";
 const RightSideBar = () => {
   return (
-    <div className="max-md:hidden max-xl:right-0 max-xl:w-[30vw] r w-[25vw] h-[calc(80vh+120px)] absolute right-[8vw] p-[15px]  no-scrollbar overflow-y-scroll">
+    <div className="max-md:hidden max-xl:right-0 max-xl:w-[30vw] r w-[25vw] p-[15px]  no-scrollbar overflow-y-scroll">
       <RightSideBarContainer title={"Your Communitites"} />
       <RightSideBarContainer2 title={"Your Tags"} />
     </div>
