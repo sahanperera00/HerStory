9d7414aeaae86ef42a6db4173614d3d27363e008
ghostfacegangsta <chanukya.meshini.18@gmail.com>
2023-05-19 16:06:42 +0530

    Added date range

--- backend/routers/Forum/post.routes.js
+++ backend/routers/Forum/post.routes.js
@@ -1,30 +1,32 @@
-import express from 'express';
-import cors from 'cors';
-import { authenticate } from '../../middleware/authentication.js';
+import express from "express";
+import cors from "cors";
+import { authenticate } from "../../middleware/authentication.js";
 import {
-	getPosts,
-	createPost,
-	getPost,
-	updatePost,
-	deletePost,
-	likePost,
+  getPosts,
+  createPost,
+  getPost,
+  getDateRangePosts,
+  updatePost,
+  deletePost,
+  likePost,
   UnlikePost,
   dislikePost,
   undislikePost,
-  makeComment
-} from '../../controllers/Forum/postController.js';
+  makeComment,
+} from "../../controllers/Forum/postController.js";
 
 const router = express.Router();
 
-router.get('/', authenticate, getPosts);
-router.get('/:id', authenticate, getPost);
-router.post('/', authenticate, createPost);
-router.put('/like/:postId', authenticate, likePost);
-router.put('/unlike/:postId', authenticate, UnlikePost);
-router.put('/dislike/:postId', authenticate, dislikePost);
-router.put('/undislike/:postId', authenticate, undislikePost);
-router.put('/comment/:postId', authenticate, makeComment);
-router.put('/:id', authenticate, updatePost);
-router.delete('/:id', authenticate, deletePost);
+router.get("/", authenticate, getPosts);
+router.get("/:id", authenticate, getPost);
+router.get("/date/:DS/:DE", getDateRangePosts);
+router.post("/", authenticate, createPost);
+router.put("/like/:postId", authenticate, likePost);
+router.put("/unlike/:postId", authenticate, UnlikePost);
+router.put("/dislike/:postId", authenticate, dislikePost);
+router.put("/undislike/:postId", authenticate, undislikePost);
+router.put("/comment/:postId", authenticate, makeComment);
+router.put("/:id", authenticate, updatePost);
+router.delete("/:id", authenticate, deletePost);
 
 export default router;
